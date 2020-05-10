5044e499fad77e27a14137ecd53f8214066a6c11
Shane Rhys Chua <s10194712@connect.np.edu.sg>
2020-05-10 18:13:09 +0800

    pract 3 (incomplete)

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -1,7 +1,7 @@
 package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.ImageView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView babyImage = (ImageView) findViewById(R.id.babyImage);
+        babyImage.setImageResource(R.drawable.cute_baby);
     }
 }
